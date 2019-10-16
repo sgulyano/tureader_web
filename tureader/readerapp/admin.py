ea88b154edcf370af7d3c360b24050979215c703
Phanuphong Siriphongwatana <phanuphongsiriphongwatana@Phanuphongs-MacBook-Pro.local>
2019-10-17 00:46:07 +0700

    update template

--- tureader/readerapp/admin.py
+++ tureader/readerapp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Reader
 
-admin.site.register(Reader)
+class ReaderAdmin (admin.ModelAdmin):
+    fields=('rank','phd','firstname','lastname','edu','spc','spc_field','contact','uni','field','tel','email','status','source')
+    list_display=('id','rank','phd','firstname','lastname','edu','spc','spc_field','contact','uni','field','tel','email','status','source') 
+    #list_editable=('rank','phd','firstname','lastname','edu','spc','spc_field','contact','uni','field','tel','email','status','source')
+admin.site.register(Reader,ReaderAdmin)
+
+
