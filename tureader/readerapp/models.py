28f42c3d7a435be8c15e7852669413dacd276a30
Sarun Gulyanon <sarung@sci.tu.ac.th>
2020-01-22 10:31:26 +0700

    Fix field name in admin page

--- tureader/readerapp/models.py
+++ tureader/readerapp/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Reader(models.Model):
-    rank = models.CharField(max_length=100, default='-')
-    phd = models.CharField(max_length=100)
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    edu = models.TextField()  #คุณวุติ
-    spc = models.TextField() #ความเชี่ยวชาญ
-    contact = models.TextField()
-    uni = models.CharField(max_length=200) #มหาลัย
-    field = models.CharField(max_length=100)
-    tel = models.TextField()
-    email = models.EmailField()
-    status = models.CharField(max_length=100)
-    spc_field = models.TextField()
-    source = models.CharField(max_length=100)
+    rank = models.CharField('ตำแหน่งทางวิชาการ', max_length=100, default='-')
+    phd = models.CharField('ตำแหน่ง ป.เอก', max_length=100)
+    firstname = models.CharField('ชื่อ', max_length=100)
+    lastname = models.CharField('สกุล', max_length=100)
+    edu = models.TextField('คุณวุฒิ')
+    spc = models.TextField('ความเชี่ยวชาญ')
+    contact = models.TextField('สถานที่ติดต่อ')
+    uni = models.CharField('มหาวิทยาลัย', max_length=200) #มหาลัย
+    field = models.CharField('สาขาวิชา', max_length=100)
+    tel = models.TextField('โทร.')
+    email = models.EmailField('อีเมล')
+    status = models.CharField('สถานะ', max_length=100)
+    spc_field = models.TextField('สาขาวิชาตาม ก.พ.อ.')
+    source = models.CharField('แหล่งที่มา', max_length=100)
     def __str__(self):
         return self.firstname+' '+self.lastname
