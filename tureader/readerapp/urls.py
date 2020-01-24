6fc9b5e55f63adffeb131987716b17ebc88101a8
Phanuphong Siriphongwatana <phanuphongsiriphongwatana@Phanuphongs-MacBook-Pro.local>
2020-01-24 22:18:05 +0700

    Fix WARNINGS

--- tureader/readerapp/urls.py
+++ tureader/readerapp/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'^export/xls/$', views.export_page_xls, name='export_page_xls'),
-    path(r'^export/xls/k$', views.export_all_xls, name='export_all_xls')
+    re_path(r'^export/xls/$', views.export_page_xls, name='export_page_xls'),
+    re_path(r'^export/xls/k$', views.export_all_xls, name='export_all_xls')
 ] 
