4d5678bf0030c84342d98be4a8cdc7d0857f60bd
Sarun Gulyanon <sgulyano@outlook.com>
2020-01-28 21:27:58 +0700

    Add docker-compose for nginx+gunicorn

--- tureader/run.sh
+++ tureader/run.sh
@@ -2,3 +2,4 @@ python manage.py makemigrations readerapp
 python manage.py migrate
 python manage.py load res/readerlist.csv 
 python manage.py createsuperuser
+python manage.py collectstatic
--- tureader/tureader/settings.py
+++ tureader/tureader/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_cy@07_qg2kv4=y&xcm-9i*50$e1mym&bzgi1a2y)x0m(&#*ea'
+SECRET_KEY = int(os.environ.get("DEBUG", default=0))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.24', '127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
@@ -125,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
